8975495238708585a2089f2936b91e8d7af3b626
rururu <ynk618172@gmail.com>
2018-10-29 02:41:50 +0000

    add session model

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,9 @@ Rails.application.routes.draw do
   root to: 'tasks#index'
   resources :tasks
   
-  get 'login', to: 'session#new'
-  post 'login', to: 'session#create'
-  delete 'logout', to: 'session#destroy'
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
   
   get 'signup', to: 'users#new'
   resources :users
