c9789c3b01360f335fcc33c41f96bbb690e90537
rururu <ynk618172@gmail.com>
2018-10-29 02:24:15 +0000

    add user model

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
   root to: 'tasks#index'
   resources :tasks
+  
+  get 'login', to: 'session#new'
+  post 'login', to: 'session#create'
+  delete 'logout', to: 'session#destroy'
+  
+  get 'signup', to: 'users#new'
+  resources :users
 end
